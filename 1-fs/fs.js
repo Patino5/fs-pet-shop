fa67368e18435bf94d9583d544a9d9294ebed171
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-11-03 03:01:22 +1000

    added create f(x) and error handling

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -16,6 +16,10 @@ if(process.argv.length <= 2) {
     switch(dbCommand){
         case 'read':
             showPets(args[1])
+            break;
+        case 'create':
+            createPet(args[1], args[2], args[3])
+            break;
     }
 }
 
@@ -27,8 +31,39 @@ function showPets(index){
         } else if (index === undefined){
         petData = JSON.parse(data)
         console.log(`petData`, petData);
-        } else {
+        } else if (0 > index || petData.length <= index){
             console.log(`petData at Index:`, JSON.parse(data)[index]);
+        } else {
+            console.log(`Usage: node fs.js read INDEX`)
         }
+    })
+}
+
+function createPet(petAge, petKind, petName){
+    if([petAge,petKind, petName].includes(undefined)) {
+        console.log(`Usage: node ./js.js create <age> <kind> <name>`)
+        process.exit(9)
+    }
+    fs.readFile(dbPath, 'utf8', (error, data) => {
+        if (error) {
+            console.log(`Error reading file`, error)
+            process.exit(9)
+        }
+        petData = JSON.parse(data)
+        petData[petData.length] =  {
+            age: Number(petAge),
+            kind: petKind,
+            name: petName
+        }
+
+        console.log(petData)
+        fs.writeFile(dbPath, JSON.stringify(petData),
+        (error) => {
+            if(error){
+                console.error(`Usage: node fs.js read INDEX`)//Usage: node fs.js read INDEX
+                process.exit(9);
+            }
+            console.log('file write success')
+        })
     })
 }
