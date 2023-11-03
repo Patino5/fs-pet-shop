0fdda9557391c08fe81ea385a53a7cc0a323527f
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-11-04 00:40:01 +1000

    added POST f(x)

--- 3-express/express.js
+++ 3-express/express.js
@@ -3,66 +3,68 @@ import fs from "fs";
 
 const PORT = 8000;
 
-// let pets = undefined
+let pets = undefined
 
-// const start = () =>{
-//     const app = express()
-//     app.use(express.json())
+const start = () =>{
 
-//     pets = importPetsFromDatabase()
+    pets = importPetsFromDatabase()
 
-//     app.post("/pets", create)
-//     app.get("/pets/:petIndex?", read)
-//     app.use(send404)
+    const app = express()
+    app.use(express.json())
 
-//     app.listen(PORT, () => {
-//         console.log(`Server is running on port ${PORT}`)
-//     })
-// }
+    app.post("/pets", create)
+    app.get("/pets/:petIndex?", read)
+    app.use(send404)
 
-// const importPetsFromDatabase = () => {
-// const data = fs.readFileSync("./pets.json")
-// return JSON.parse(data)
-// }
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+}
 
-// const updateDatabase = () => {
-// fs.writeFileSync("./pets.json", JSON.stringify(pets))
-// }
+const importPetsFromDatabase = () => {
+const data = fs.readFileSync("./pets.json")
+return JSON.parse(data)
+}
 
-// const create = (req, res) => {
-// const pet = req.body
-// if ([pet.age, pet.kind, pet.name].includes(undefined)) {
-//     res.status(500)
-//     res.send("Failed.")
-// } else {
-//     pets.push(pet)
-//     updateDatabase()
-//     res.send("Success.")
-// }
-// }
+const updateDatabase = () => {
+fs.writeFileSync("./pets.json", JSON.stringify(pets))
+}
 
-// const read = (req, res) => {
-// const { petIndex } = req.params
-// if (petIndex === undefined) {
-//     res.status(200)
-//     res.json(pets)
-// } else {
-//     const index = parseInt(petIndex)
-//     if (index >= 0 && index < pets.length) {
-//         res.status(200)
-//         res.json(pets[index])
-//     } else {
-//         res.send("Not Found.")
-//     }
-// }
-// }
+const create = (req, res) => {
+const pet = req.body
+if ([pet.age, pet.kind, pet.name].includes(undefined)) {
+    res.status(500)
+    res.send("Failed.")
+} else {
+    pets.push(pet)
+    updateDatabase()
+    res.send("Success.")
+}
+}
 
-// const send404 = (req, res) => {
-// res.status(404)
-// res.send("404 Error - Page Not Found")
-// }
+const read = (req, res) => {
+const { petIndex } = req.params
+if (petIndex === undefined) {
+    res.status(200)
+    res.json(pets)
+} else {
+    const index = parseInt(petIndex)
+    if (index >= 0 && index < pets.length) {
+        res.status(200)
+        res.json(pets[index])
+    } else {
+        res.send("Not Found.")
+    }
+}
+}
 
-// start()
+
+const send404 = (req, res) => {
+res.status(404)
+res.send("404 Error - Page Not Found")
+}
+
+start()
 
 // take a request that checks if request has test header
 
@@ -71,38 +73,38 @@ const PORT = 8000;
 // forward to route and allow route to handle the error
 
 
-app.use(function(req, res, next) {
-    if(!req.headers.testheader) {
-        next()
-    } 
-    req.headers.testheader = 999
-    next()
-})
+// app.use(function(req, res, next) {
+//     if(!req.headers.testheader) {
+//         next()
+//     } 
+//     req.headers.testheader = 999
+//     next()
+// })
 
 
-app.get('/', function(req, res, next) {
-    const number = req.headers.testheader
-    if(number) {
-        console.log(number)
-        res.json({message: 'Authorized'})
-    } else {
-        next({message: 'Token not present', status: 401})
-    }
-})
+// app.get('/', function(req, res, next) {
+//     const number = req.headers.testheader
+//     if(number) {
+//         console.log(number)
+//         res.json({message: 'Authorized'})
+//     } else {
+//         next({message: 'Token not present', status: 401})
+//     }
+// })
 
-app.get('/test', function(req, res) {
-    res.json({message: 'Hit the /test route'})
-})
-
-
-app.use((err, req, res, next) => {
-    res.status(err.status).json({err: err.message})
-})
+// app.get('/test', function(req, res) {
+//     res.json({message: 'Hit the /test route'})
+// })
 
 
-app.listen(PORT, function() {
-    console.log(`Listening on port: ${PORT} for requests...`)
-})
+// app.use((err, req, res, next) => {
+//     res.status(err.status).json({err: err.message})
+// })
+
+
+// app.listen(PORT, function() {
+//     console.log(`Listening on port: ${PORT} for requests...`)
+// })
 
 // // :id 
 // // req.params.id
