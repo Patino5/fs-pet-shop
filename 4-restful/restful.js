8cc52fadc810e40bf41714b2c6dab086b8b41c38
Patino5 <106916823+Patino5@users.noreply.github.com>
2023-11-10 07:46:00 +1000

    added Get one and Get all routes

--- 4-restful/restful.js
+++ 4-restful/restful.js
@@ -1,78 +1,130 @@
-import express from 'express';
-import fs from 'fs';
+// import express from 'express';
+// import fs from 'fs';
 
-const PORT = 3000;
-const petDatabase = "./pets.json";
-const app = express();
+// const PORT = 3000;
+// const petDatabase = "./pets.json";
+// const app = express();
 
-const petData = fs.readFileSync(petDatabase, 'utf8');
-const pets = JSON.parse(petData);
+// const petData = fs.readFileSync(petDatabase, 'utf8');
+// const pets = JSON.parse(petData);
 
-app.use(express.json());
+// app.use(express.json());
 
-app.get('/pets', (req, res) => {
-res.status(200).json(pets);
-});
+// app.get('/pets', (req, res) => {
+// res.status(200).json(pets);
+// });
 
-app.get('/pets/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    if (id >= 0 && id < pets.length) {
-        res.status(200).json(pets[id]);
-    } else {
-        res.status(404).send('Not Found');
-    }
-});
+// app.get('/pets/:id', (req, res) => {
+//     const id = parseInt(req.params.id);
+//     if (id >= 0 && id < pets.length) {
+//         res.status(200).json(pets[id]);
+//     } else {
+//         res.status(404).send('Not Found');
+//     }
+// });
 
-app.post('/pets', (req, res) => {
-const pet = req.body;
-if ([pet.name, pet.kind, pet.age].includes(undefined)) {
-    res.status(400).send("Bad Request");
-}else{
-    pets.push(pet);
-    fs.writeFileSync('./pets.json', JSON.stringify(pets))
-    res.status(201).json(pet);
-}
+// app.post('/pets', (req, res) => {
+// const pet = req.body;
+// if ([pet.name, pet.kind, pet.age].includes(undefined)) {
+//     res.status(400).send("Bad Request");
+// }else{
+//     pets.push(pet);
+//     fs.writeFileSync('./pets.json', JSON.stringify(pets))
+//     res.status(201).json(pet);
+// }
+// })
+
+// app.put('/pets/:id', (req, res) => {
+//     const updatedPetData = req.body;
+//     const index = parseInt(req.params.id);
+  
+//     if (index >= 0 && index < pets.length) {
+//       const updatedPet = pets[index];
+  
+//       if ([updatedPetData.name, updatedPetData.kind, updatedPetData.age].includes(undefined)) {
+//         res.status(400).send("Bad Request");
+//       } else {
+//         updatedPet.name = updatedPetData.name;
+//         updatedPet.kind = updatedPetData.kind;
+//         updatedPet.age = updatedPetData.age;
+  
+//         fs.writeFileSync(petDatabase, JSON.stringify(pets));
+  
+//         res.status(200).json(updatedPet);
+//       }
+//     } else {
+//       res.status(404).send("Pet not found");
+//     }
+//   });
+
+//   app.delete('/pets/:id', (req, res) => {
+//     const index = parseInt(req.params.id);
+  
+//     if (index >= 0 && index < pets.length) {
+//       const deletedPet = pets.splice(index, 1);
+//       fs.writeFileSync(petDatabase, JSON.stringify(pets));
+//       res.status(200).json(deletedPet)
+//   }else{
+//     res.status(404).send("Pet not found")
+//   }
+// })
+  
+// app.use((req, res) => {
+//     res.status(404).send('Bad Request');
+// })
+
+// app.listen(PORT, () => {
+// console.log(`Listening on port: ${PORT}`)
+// });
+
+const express = require('express')
+const pg = require('pg')
+const app = express() 
+const {Pool} = pg
+
+const PORT = 3000
+
+const pool = new Pool ({
+  user: 'ryanpatino',
+  database: 'petsdb',
+  password: '',
+  host: 'localhost',
+  port: 5432
 })
 
-app.put('/pets/:id', (req, res) => {
-    const updatedPetData = req.body;
-    const index = parseInt(req.params.id);
-  
-    if (index >= 0 && index < pets.length) {
-      const updatedPet = pets[index];
-  
-      if ([updatedPetData.name, updatedPetData.kind, updatedPetData.age].includes(undefined)) {
-        res.status(400).send("Bad Request");
-      } else {
-        updatedPet.name = updatedPetData.name;
-        updatedPet.kind = updatedPetData.kind;
-        updatedPet.age = updatedPetData.age;
-  
-        fs.writeFileSync(petDatabase, JSON.stringify(pets));
-  
-        res.status(200).json(updatedPet);
-      }
-    } else {
-      res.status(404).send("Pet not found");
-    }
-  });
+// middlewares
+// app.use(express.static('public'))
 
-  app.delete('/pets/:id', (req, res) => {
-    const index = parseInt(req.params.id);
-  
-    if (index >= 0 && index < pets.length) {
-      const deletedPet = pets.splice(index, 1);
-      fs.writeFileSync(petDatabase, JSON.stringify(pets));
-      res.status(200).json(deletedPet)
-  }else{
-    res.status(404).send("Pet not found")
+
+
+app.get('/api/pets', async (req, res) => {
+  try {
+    const {rows} = await pool.query(`SELECT * FROM pets;`)
+    res.send({rows})
+  } catch (error) {
+    console.log(error)
+    res.json(error)
   }
 })
-  
-app.use((req, res) => {
-    res.status(404).send('Bad Request');
+
+app.get('/api/pets/:id', async (req, res) => {
+  try {
+    const id = parseInt(req.params.id)
+    const {rows} = await pool.query(`SELECT * FROM pets WHERE id = ${id};`)
+
+    if(rows.length === 1) {
+      res.status(200).json(rows)
+    } else {
+      res.status(404).send('Not Found')
+    }
+  } catch (error) {
+    console.log(error)
+    res.json(error)
+  }
 })
 
+
+
 app.listen(PORT, () => {
-console.log(`Listening on port: ${PORT}`)
-});
+  console.log('listening');
+})
